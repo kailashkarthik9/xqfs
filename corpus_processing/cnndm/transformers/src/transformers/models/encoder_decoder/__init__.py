7acfaa4aecc9b6908c14411b1bd5c657d4ab483b
Kailash Karthik S <kailashkarthik9@gmail.com>
2020-11-19 11:42:41 -0500

    moving transformers repository

--- corpus_processing/cnndm/transformers/src/transformers/models/encoder_decoder/__init__.py
+++
@@ -1,9 +0,0 @@
-# flake8: noqa
-# There's no way to ignore "F401 '...' imported but unused" warnings in this
-# module, but to preserve other warnings. So, don't check this module at all.
-
-from .configuration_encoder_decoder import EncoderDecoderConfig
-from ...file_utils import is_torch_available
-
-if is_torch_available():
-    from .modeling_encoder_decoder import EncoderDecoderModel
--- corpus_processing/cnndm/transformers/src/transformers/models/herbert/__init__.py
+++
@@ -1,9 +0,0 @@
-# flake8: noqa
-# There's no way to ignore "F401 '...' imported but unused" warnings in this
-# module, but to preserve other warnings. So, don't check this module at all.
-
-from .tokenization_herbert import HerbertTokenizer
-from ...file_utils import is_tokenizers_available
-
-if is_tokenizers_available():
-    from .tokenization_herbert_fast import HerbertTokenizerFast
